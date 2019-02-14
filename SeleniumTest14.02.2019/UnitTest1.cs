32c111a10ec4992a96a2360ff2de931e98f978e7
igor27w <igor.piotr.grabowski@gmail.com>
2019-02-14 22:35:05 +0100

    First commit

---
+++ SeleniumTest14.02.2019/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+
+
+namespace SeleniumTest14._02._2019
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            IWebDriver driver = GetChromeDriver();
+            driver.Navigate().GoToUrl("https://www.ultimateqa.com");
+
+
+        }
+
+        private IWebDriver GetChromeDriver()
+        {
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new ChromeDriver(outPutDirectory);
+        }
+
+
+
+    }
+}
+
+
+
+
+
+
